d7ec498bdeb1bbb2038737c07d0c9c6fecd45b2a
e41162068iljamiatinzannah <37361566+e41162068iljamiatinzannah@users.noreply.github.com>
2018-05-18 09:17:46 +0700

    IL

---
+++ bismillah/app/src/main/java/com/example/client/bismillah/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.client.bismillah;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_logout;
+    TextView txt_id, txt_username;
+    String nim, username;
+    SharedPreferences sharedpreferences;
+
+    public static final String TAG_ID = "nim";
+    public static final String TAG_USERNAME = "username";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txt_id = (TextView) findViewById(R.id.txt_id);
+        txt_username = (TextView) findViewById(R.id.txt_username);
+        btn_logout = (Button) findViewById(R.id.btn_logout);
+
+        sharedpreferences = getSharedPreferences(login.my_shared_preferences, Context.MODE_PRIVATE);
+
+        nim = getIntent().getStringExtra(TAG_ID);
+        username = getIntent().getStringExtra(TAG_USERNAME);
+
+        txt_id.setText("ID : " + nim);
+        txt_username.setText("USERNAME : " + username);
+
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                // update login session ke FALSE dan mengosongkan nilai id dan username
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                editor.putBoolean(login.session_status, false);
+                editor.putString(TAG_ID, null);
+                editor.putString(TAG_USERNAME, null);
+                editor.commit();
+
+                Intent intent = new Intent(MainActivity.this, login.class);
+                finish();
+                startActivity(intent);
+            }
+        });
+
+    }
+}
